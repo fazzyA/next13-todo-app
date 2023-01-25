c97a9121125751076c849b009a729dd56d6a9518
fazzyA <faz.pak@gmail.com>
2023-01-25 14:53:44 +0500

    UI

--- app/add-todo.tsx
+++ app/add-todo.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Row } from 'react-bootstrap';
 
 const TodoForm = (props) => {
   return (
+    <div className="m-4 d-flex justify-content-center">
+      <Row><label className="pe-4 flex-sm-row text-nowrap">My List</label></Row>
     <div className="TodoForm-container">
       <form className="TodoForm" onSubmit={props.onSubmit}>
         <input
@@ -11,18 +15,17 @@ const TodoForm = (props) => {
           value={props.value}
           onChange={props.onChange}
           ref={props.reference}
-          required
-        />
+          required />
         <div className="btn-container">
-          <button className="form-btn" type="submit">
+          <Button variant="success" type="submit" size="sm">
             {!props.isEditing ? "Add a task" : "Edit Task"}
-          </button>
-          <button className="form-btn" type="button" onFocus={props.onClick}>
+          </Button>
+          <Button className="ms-2" variant="danger" type="button" size="sm" onFocus={props.onClick}>
             {!props.isEditing ? "Clear tasks" : "Cancel"}
-          </button>
+          </Button>
         </div>
       </form>
-    </div>
+    </div></div>
   );
 };
 
